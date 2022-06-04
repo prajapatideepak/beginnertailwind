b80dc9d8cef13aad1d1449a18f84391415b65111
Deepak Prajapati <prajapatideepak073@gmail.com>
2022-06-04 12:41:06 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require("tailwindcss/colors")
+module.exports = {
+  mode: 'jit',
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    // classes that are generated dynamically, e.g. `rounded-${size}` and must
+    // be kept
+    safeList: [],
+    content: [
+      './index.html',
+      './src/**/*.{vue,js,ts}',
+      // etc.
+    ],
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors:{
+        orange:colors.orange,
+        teal:colors.teal,
+        "light-blue":colors.lightBlue,
+      }
+    },
+  },
+}
