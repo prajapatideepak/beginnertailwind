7c6bdc41611dfc09b5042c7c00d28df4652843a6
Deepak Prajapati <prajapatideepak073@gmail.com>
2022-06-04 12:49:04 +0530

    error fix in npm

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const colors = require("tailwindcss/colors")
 module.exports = {
   mode: 'jit',
   purge: {
-    enabled: process.env.NODE_ENV === 'production',
     // classes that are generated dynamically, e.g. `rounded-${size}` and must
     // be kept
     safeList: [],
